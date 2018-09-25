332d8a67f5f0ef5ff93fea78ef5273ce80d05590
ray <rayjun0412@gmail.com>
2018-09-25 19:29:27 +0800

    Update

---
+++ src/main/java/com/learning/spring/springioc/Main.java
@@ -0,0 +1,23 @@
+package com.learning.spring.springioc;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		BeanFactory beanFactory = new ClassPathXmlApplicationContext("spring-ioc.xml").getBeanFactory();
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-ioc.xml");
+
+
+		SpringIoc springIocFromBeanFactory = (SpringIoc) beanFactory.getBean("springIoc");
+		SpringIoc springIocFromApplicationContext = (SpringIoc) applicationContext.getBean("springIoc");
+
+
+
+		springIocFromBeanFactory.hellIoc();
+		springIocFromApplicationContext.hellIoc();
+	}
+}
---
+++ src/main/java/com/learning/spring/springioc/SpringIoc.java
@@ -0,0 +1,13 @@
+package com.learning.spring.springioc;
+
+public class SpringIoc {
+
+
+	public SpringIoc() {
+		System.out.println("Construct invoke");
+	}
+
+	public void hellIoc() {
+		System.out.println("Hello IOC");
+	}
+}
